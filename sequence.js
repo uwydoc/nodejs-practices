50766eaea8b5e6a01479235418b7a21e9b79d1c5
uwydoc <uwydoc@gmail.com>
2014-04-03 02:33:30 +0800

    Fix bug with arguments parsing, working now

--- sequence.js
+++ sequence.js
@@ -73,7 +73,7 @@ Deferred.prototype.callback = function() {
 var smooth = function(method) {
   return function() {
     var deferred = new Deferred();
-    var args = Array.prototype.slice.call(arguments, 1);
+    var args = Array.prototype.slice.call(arguments, 0);
     args.push(deferred.callback());
     method.apply(null, args);
     return deferred.promise;
@@ -86,6 +86,10 @@ var fs = require('fs');
 var readFile = smooth(fs.readFile);
 readFile('data/file1.txt', 'utf8').then(function(data) {
   return readFile(data.trim(), 'utf8');
+}, function(err) {
+  throw err;
 }).then(function(data) {
   console.log(data);
+}, function(err) {
+  throw err;
 });
